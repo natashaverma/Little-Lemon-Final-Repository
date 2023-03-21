6ce672d437dc4763d0d5970231f0764dd73c112c
Natasha Verma <naverma@adobe.com>
2023-03-21 21:32:34 +0530

    Adding a reserve table button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Food from '../assets/Logo.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
+
     return (
         <header>
             <div className="left-side">
@@ -19,6 +22,9 @@ const Header = () => {
                     src={Food}
                     alt="our cook holding a tablet with delicoues baguettes"
                 />
+            </div>
+            <div onClick={() => navigate("/reservations")} className="hero-button">
+                <h4 data-testid="reserve-button">Reserve a Table</h4>
             </div>
         </header>
     )
